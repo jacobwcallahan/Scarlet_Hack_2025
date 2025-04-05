1e84f514a07d6d1400d9207014e73147bd0f2579
korijamal <korijamal.me@gmail.com>
2025-04-05 18:18:36 -0500

    files directory changed little bit

---
+++ frontend/src/component/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+function App() {
+  console.log("====================================");
+  console.log("rendering");
+  console.log("====================================");
+  return <div className="App">
+     <h1 className="underline">
+    💖 Hello world!
+    </h1>
+     <p>Welcome to your Electron application.</p>
+  </div>;
+}
+
+export default App;
---
+++ frontend/src/index.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+// import './index.css';
+// import App from "./app";
+import App from "./component/App.jsx";
+
+const root = createRoot(document.body);
+root.render(
+  <>
+    <App />
+  </>
+);
