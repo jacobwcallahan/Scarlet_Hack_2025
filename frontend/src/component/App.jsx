b637966f96343b77aa7537a1b22c0e7e341a7b99
korijamal <korijamal.me@gmail.com>
2025-04-05 18:24:46 -0500

    css is added

--- frontend/src/component/App.jsx
+++ frontend/src/component/App.jsx
@@ -4,7 +4,7 @@ function App() {
   console.log("rendering");
   console.log("====================================");
   return <div className="App">
-     <h1 className="underline">
+    <h1 className="text-3xl underline" style={{backgroundColor:'red'}}>
     💖 Hello world!
     </h1>
      <p>Welcome to your Electron application.</p>
