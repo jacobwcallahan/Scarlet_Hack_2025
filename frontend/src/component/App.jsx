4e3da6ad8d591334b26b3babcb214babd38b3624
korijamal <korijamal.me@gmail.com>
2025-04-06 01:07:21 -0500

    Frontend is done

--- frontend/src/component/App.jsx
+++ frontend/src/component/App.jsx
@@ -1,14 +1,50 @@
-import React from "react";
-function App() {
-  console.log("====================================");
-  console.log("rendering");
-  console.log("====================================");
-  return <div className="App">
-    <h1 className="text-3xl underline" style={{backgroundColor:'red'}}>
-    💖 Hello world!
-    </h1>
-     <p>Welcome to your Electron application.</p>
-  </div>;
-}
+import React, { useState, useEffect } from "react";
+import Typewriter from "typewriter-effect";
+import "./App.css";
+
+const App = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const testText = 'You will need to bundle the javascript before it can be used, this can be done using npm run build:dev for development or npm run build:prod for production.You will need to bundle the javascript before it can be used, this can be done using npm run build:dev for development or npm run build:prod for production.You will need to bundle the javascript before it can be used, this can be done using npm run build:dev for development or npm run build:prod for production.'
+  
+  return (
+    <div className="app-container">
+      <div className="leftGradientbox">
+        <button className="primary-btn">Translation</button>
+
+        <div className="typing-container">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString(testText)
+                .callFunction(() => {
+                  console.log("String typed out!");
+                })
+                .pauseFor(2500)
+                // .deleteAll()
+                .callFunction(() => {
+                  console.log("All strings were deleted");
+                })
+                .start();
+            }}
+          />
+        </div>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "3rem" }}>
+        <div
+          // }}
+          className="rightGradientBox"
+        >
+          <button className="primary-btn">Emergency Words</button>
+        </div>
+        <div
+          // }}
+          className="rightGradientBox"
+        >
+          <button className="primary-btn">Caller Information</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
