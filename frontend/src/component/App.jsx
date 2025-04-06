026d8f14f19a4cd00a359d293bc10cb7021f2702
korijamal <korijamal.me@gmail.com>
2025-04-06 01:07:47 -0500

    Frontend is done

--- frontend/src/component/App.jsx
+++ frontend/src/component/App.jsx
@@ -3,9 +3,8 @@ import Typewriter from "typewriter-effect";
 import "./App.css";
 
 const App = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
   const testText = 'You will need to bundle the javascript before it can be used, this can be done using npm run build:dev for development or npm run build:prod for production.You will need to bundle the javascript before it can be used, this can be done using npm run build:dev for development or npm run build:prod for production.You will need to bundle the javascript before it can be used, this can be done using npm run build:dev for development or npm run build:prod for production.'
-  
+
   return (
     <div className="app-container">
       <div className="leftGradientbox">
